bca413815ab79b8eac0c0d824f2325b99f075b49
Kármán Ádám <31824068+ADAM424526@users.noreply.github.com>
2022-10-14 13:51:02 +0200

    Add files via upload

---
+++ sum max min.py
@@ -0,0 +1,33 @@
+lista = [10,4,5,7,2,3,6,13]
+legkisebb = lista[0]
+legnagyobb = lista[0]
+def mini(lista):
+        legkisebb = lista[0]
+        for szam in lista:
+            if szam < legkisebb:
+                legkisebb = szam
+        return legkisebb
+    
+    
+
+
+
+lista = [10,4,5,7,2,3,6,13]
+legkisebb = lista[0]
+
+def maxi(lista):
+        legnagyobb = lista[0]
+        for szam in lista:
+            if szam > legnagyobb:
+                legnagyobb = szam
+        return legnagyobb
+
+
+def suma(lista):
+    osszeg = 0
+    for szam in lista:
+        osszeg = osszeg + szam
+    return osszeg
+
+
+    
